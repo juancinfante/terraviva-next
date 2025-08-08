49a9adac9e1c045e1b79b63755d8f32fd1f7250d
juancinfante <juaninfantejj@gmail.com>
2025-08-08 17:55:47 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
   /* config options here */
 };
 
 export default nextConfig;
+
+
